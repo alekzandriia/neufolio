f9827d64661bc60439d71e61661ad5c3435ae25c
Alekzandriia <alekzoreillyz@me.com>
2022-08-27 19:44:39 -0400

    Changed dark mode colors

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ function themeToggle () {
     const root = document.documentElement
     root.classList.toggle('dark')
   }
-  
-  localStorage.setItem('theme', 'dark');
-  
-  //accessed the user's machine theme
-  localStorage.getItem('theme');
